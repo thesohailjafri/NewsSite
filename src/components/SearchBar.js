627d0f4ea09e607cfc7a485af53e0dcd073a7584
Akashdeepgupta <akashdeepg2001@gmail.com>
2022-10-09 22:11:57 +0530

    cc

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,43 +2,6 @@ import React, { useRef, useCallback, useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import { useAppContext } from '../context'
 
-function SearchBar() {
-  const ref = useRef(null)
-  const { searchFromSearchBar } = useAppContext()
-  const onSearchSubmit = async (e) => {
-    e.preventDefault()
-    if (ref.current.value) {
-      await searchFromSearchBar(ref.current.value)
-      ref.current.value = null
-    }
-  }
-  return (
-    <form id="searchForm" className="flex my-4">
-      <input
-        ref={ref}
-        className="py-3 px-4 text-md rounded mr-4 flex-grow"
-        type="text"
-        placeholder="Search News"
-        id="searchbar"
-        onKeyPress={(e) => {
-          if (e.key === 'Enter') {
-            e.preventDefault()
-            onSearchSubmit(e)
-          }
-        }}
-      />
-      <button
-        className=" min-w-150 btn sm:min-w-100"
-        type="button"
-        for="searchbar"
-        value="Search"
-        onClick={(e) => onSearchSubmit(e)}
-      >
-        <FaSearch className="inline-block mr-2" />
-        Search
-      </button>
-    </form>
-  )
-}
+function SearchBar() {}
 
 export default SearchBar
